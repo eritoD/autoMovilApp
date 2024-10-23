ebaf163b27f0afb14419dc6abdcf9a744b9bcde8
ToroDanger <bryanpmg90@gmail.com>
2024-10-23 13:55:06 -0300

    avances

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA1VziinqonyjnGiBcE6CuZVLEO-WbnO4U",
+    authDomain: "automovilapp-421d2.firebaseapp.com",
+    projectId: "automovilapp-421d2",
+    storageBucket: "automovilapp-421d2.appspot.com",
+    messagingSenderId: "914402650129",
+    appId: "1:914402650129:web:631cc4a4dbf8b74acda9b0"
+  }
 };
